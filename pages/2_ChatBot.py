e4b6454d7ceef479c15098ed1864ffbf19b41945
26dishag <103006573+26dishag@users.noreply.github.com>
2023-12-03 21:31:50 -0800

    removed sidebar

--- pages/2_ChatBot.py
+++ pages/2_ChatBot.py
@@ -2,17 +2,19 @@ import openai
 import streamlit as st
 
 
-with st.sidebar:
-    st.title("🤖💬 OpenAI Chatbot")
-    if "OPENAI_API_KEY" in st.secrets:
-        st.success("API key already provided!", icon="✅")
-        openai.api_key = st.secrets["OPENAI_API_KEY"]
-    else:
-        openai.api_key = st.text_input("Enter OpenAI API token:", type="password")
-        if not (openai.api_key.startswith("sk-") and len(openai.api_key) == 51):
-            st.warning("Please enter your credentials!", icon="⚠️")
-        else:
-            st.success("Proceed to entering your prompt message!", icon="👉")
+# with st.sidebar:
+#     st.title("🤖💬 OpenAI Chatbot")
+#     if "OPENAI_API_KEY" in st.secrets:
+#         st.success("API key already provided!", icon="✅")
+#         openai.api_key = st.secrets["OPENAI_API_KEY"]
+#     else:
+#         openai.api_key = st.text_input("Enter OpenAI API token:", type="password")
+#         if not (openai.api_key.startswith("sk-") and len(openai.api_key) == 51):
+#             st.warning("Please enter your credentials!", icon="⚠️")
+#         else:
+#             st.success("Proceed to entering your prompt message!", icon="👉")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
